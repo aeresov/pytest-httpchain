6f26b384e4a898853c174a38566b3adf962701a8
Alexander Eresov <Alexander.Eresov@soundunited.com>
2025-08-05 07:33:24 +0000

    wip

--- src/pytest_httpchain/core/__init__.py
+++ src/pytest_httpchain/core/__init__.py
@@ -1,7 +1,6 @@
 """Core components for pytest-httpchain."""
 
-from pytest_httpchain.core.collector import JsonModuleCollector
 from pytest_httpchain.core.executor import StageExecutor
 from pytest_httpchain.core.session import HTTPSessionManager
 
-__all__ = ["HTTPSessionManager", "StageExecutor", "JsonModuleCollector"]
+__all__ = ["HTTPSessionManager", "StageExecutor"]
